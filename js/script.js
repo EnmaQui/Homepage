47e49f68bf54b51c299fe1d705ff8797d7b4576e
EnmaQui <qenmanuel2@gmail.com>
2023-09-22 20:01:32 -0600

    navbar intento 1 circular

--- js/script.js
+++ js/script.js
@@ -63,3 +63,19 @@ function updateScroll() {
         scrollContainer.style.transition = '';
     }, 500);
 }
+
+
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
